449d8c7577354aa47f9c9e2b09874d3553da83b5
Anthony McLin <anthony.mclin@rosetta.com>
2017-10-19 23:42:01 -0700

    setup some unit tests

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -8,6 +8,24 @@ import plugin from '../src/plugin';
 
 const Tech = videojs.getComponent('Tech');
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+// function _getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+/**
+ * Returns a random alphanumeric string
+ *
+ * @return {string}
+ *    starting with 'testRun' and 8 ASCII letters
+ **/
+function _getRandomAlphaString() {
+  return 'testRun' + Math.random().toString(36).substring(8);
+}
+
 QUnit.test('the environment is sane', function(assert) {
   assert.strictEqual(typeof Array.isArray, 'function', 'es5 exists');
   assert.strictEqual(typeof sinon, 'object', 'sinon exists');
@@ -52,5 +70,71 @@ QUnit.test('registers itself with video.js', function(assert) {
   // assert.ok(
   //   this.player.hasClass('vjs-scene7'),
   //   'the plugin adds a class to the player'
+  // );
+});
+
+QUnit.test('canPlayType()', function(assert) {
+  const Scene7 = Tech.getTech('Scene7');
+
+  assert.expect(3);
+
+  assert.strictEqual(
+    typeof Scene7.canPlayType,
+    'function',
+    'is a function.'
+  );
+
+  assert.strictEqual(
+    Scene7.canPlayType('videojs/scene7'),
+    'probably',
+    'returns \'probably\' when the mime type is videojs/scene7.'
+  );
+
+  assert.strictEqual(
+    Scene7.canPlayType(_getRandomAlphaString()),
+    '',
+    'returns an empty string when the video mime type is not a match.'
+  );
+
+  // Not implemented because we only want to support S7 video sets
+  // assert.strictEqual(
+  //   Scene7.canPlayType('video/mp4'),
+  //   'maybe',
+  //   'returns \'maybe\' when the mime type is video/mp4.'
+  // );
+});
+
+QUnit.test('canPlaySource()', function(assert) {
+  const Scene7 = Tech.getTech('Scene7');
+
+  assert.expect(3);
+
+  assert.strictEqual(
+    typeof Scene7.canPlaySource,
+    'function',
+    'is a function.'
+  );
+
+  assert.strictEqual(
+    Scene7.canPlaySource({
+      src: 'https://example.com/video',
+      type: 'videojs/scene7'}),
+    'probably',
+    'returns \'probably\' when the mime type is videojs/scene7.'
+  );
+
+  assert.strictEqual(
+    Scene7.canPlayType({
+      src: 'https://example.com/video',
+      type: _getRandomAlphaString()}),
+    '',
+    'returns an empty string when the video mime type is not a match.'
+  );
+
+  // Not implemented because we only want to support S7 video sets
+  // assert.strictEqual(
+  //   Scene7.canPlayType('video/mp4'),
+  //   'maybe',
+  //   'returns \'maybe\' when the mime type is video/mp4.'
   // );
 });
