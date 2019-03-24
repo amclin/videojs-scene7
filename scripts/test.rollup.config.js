b2878f444209bb916d074dd70b5f087c0defc1cc
Anthony McLin <git@anthonymclin.com>
2019-03-23 18:39:43 -0700

    feat: reduce size of module through configurable browser versions
    Upgrading to Babel 7 means compiling should now respect .browserlistrc files and only compile the modules to support the browsers your project defines.
    
    BREAKING CHANGE:
    IE 10 (and possibly IE 11) support is likely dropped as a result of switching to browser-env based compiling

--- scripts/test.rollup.config.js
+++ scripts/test.rollup.config.js
@@ -45,14 +45,13 @@ export default {
       babelrc: false,
       exclude: 'node_modules/**',
       presets: [
-        ['es2015', {
+        ['@babel/preset-env', {
           loose: true,
           modules: false
         }]
       ],
       plugins: [
-        'external-helpers',
-        'transform-object-assign'
+        '@babel/plugin-transform-object-assign'
       ]
     })
   ]
